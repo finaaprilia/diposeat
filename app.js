5f8a3d4d09c3162de08457f046da5afbd5591081
finaaprilia <finaapriliaa90@gmail.com>
2026-05-31 22:56:07 +0700

    using absolute path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pool = require('./db');
+const path = require('path');
 
 const ensureReservationTable = async () => {
     try {
@@ -29,6 +30,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
